1a5d4f0071daefdd2c102ac5f068fce1adebc8ee
mpetersen49 <mpetersen49@gmail.com>
2021-04-18 11:31:29 -0500

    Got plot to work for bar graph with initial and selected sample ids

--- static/js/app.js
+++ static/js/app.js
@@ -1,24 +1,58 @@
+// portions of this code are taken directly from the instructor's office hours tutorial
 console.log("app.js loaded");
 
 // function to update bar chart
-function updateBarChart(newId) {
-    console.log(`updateBarChart(${newId})`);
+function updateBarChart(sampleId) {
+    
+    d3.json("static/data/samples.json").then(data => {
+        //console.log(data);
+
+        var samples = data.samples;
+        var resultArray = samples.filter(s => s.id == sampleId);
+        //console.log(resultArray);
+        var result = resultArray[0];
+
+        var otu_ids = result.otu_ids;
+        var otu_labels = result.otu_labels;
+        var sample_values = result.sample_values;
+
+        yticks = otu_ids.slice(0, 10).map(otuid => `OTU ${otuid}`);
+
+        var barData = {
+            x: sample_values.slice(0, 10).reverse(),
+            y: yticks.reverse(),
+            type: "bar",
+            text: otu_labels.slice(0, 10).reverse(),
+            orientation: "h"
+        };
+
+        var barArray = [barData];
+
+        var barLayout = {
+            title: "xxx"
+        };
+
+        Plotly.newPlot("bar", barArray, barLayout);
+        
+    });
+
+    
 };
 
 // function to update bubble chart
-function updateBubbleChart(newId) {
-    console.log(`updateBubbleChart(${newId})`);
+function updateBubbleChart(sampleId) {
+    console.log(`updateBubbleChart(${sampleId})`);
 };
 
 // function to update demo info
-function updateDemoInfo(newId) {
-    console.log(`updateDemoInfo(${newId})`);
+function updateDemoInfo(sampleId) {
+    console.log(`updateDemoInfo(${sampleId})`);
 };
 
-function optionChanged(id) {
-    updateBarChart(id);
-    updateBubbleChart(id);
-    updateDemoInfo(id);
+function optionChanged(newId) {
+    updateBarChart(newId);
+    updateBubbleChart(newId);
+    updateDemoInfo(newId);
 };
 
 function init() {
@@ -26,22 +60,21 @@ function init() {
     d3.json("static/data/samples.json").then(data => {
         console.log(data);
 
-        data.samples.forEach(obj => {
+        data.names.forEach(sampleId => {
             d3.selectAll("#selDataset")
                 .append("option")
-                .property("value", obj.id)
-                .text(obj.id);
+                .property("value", sampleId)
+                .text(sampleId);
         });
         
+        var id = data.names[0]
 
-        updateBarChart(data.samples[0].id);
+        updateBarChart(id);
     
-        updateBubbleChart();
+        updateBubbleChart(id);
         
-        updateDemoInfo();
+        updateDemoInfo(id);
     });
-    
-    
 
 };
 

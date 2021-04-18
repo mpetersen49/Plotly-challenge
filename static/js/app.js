063642e13c7acc072461f4f9cc6e80903c34bf98
mpetersen49 <mpetersen49@gmail.com>
2021-04-18 10:11:18 -0500

    Populated dropdown and passed ids to chart functions

--- static/js/app.js
+++ static/js/app.js
@@ -1,29 +1,52 @@
 console.log("app.js loaded");
 
-function updateBarChart() {
-    console.log("updateBarChart");
+// function to update bar chart
+function updateBarChart(newId) {
+    console.log(`updateBarChart(${newId})`);
 };
 
-function updateBubbleChart() {
-    console.log("updateBubbleChart");
+// function to update bubble chart
+function updateBubbleChart(newId) {
+    console.log(`updateBubbleChart(${newId})`);
 };
 
-function updateDemoInfo() {
-    console.log("updateDemoInfo");
+// function to update demo info
+function updateDemoInfo(newId) {
+    console.log(`updateDemoInfo(${newId})`);
 };
 
+function optionChanged(id) {
+    updateBarChart(id);
+    updateBubbleChart(id);
+    updateDemoInfo(id);
+};
 
+function init() {
+    // connect to data
+    d3.json("static/data/samples.json").then(data => {
+        console.log(data);
 
-d3.json("static/data/samples.json").then(data => {
-    console.log(data);
+        data.samples.forEach(obj => {
+            d3.selectAll("#selDataset")
+                .append("option")
+                .property("value", obj.id)
+                .text(obj.id);
+        });
+        
 
-    updateBarChart();
-
-    updateBubbleChart();
+        updateBarChart(data.samples[0].id);
     
-    updateDemoInfo();
-});
+        updateBubbleChart();
+        
+        updateDemoInfo();
+    });
+    
+    
 
+};
+
+init();
+//populate dropdown
 //update bargraph
 //update bubble chart
 //update demo info

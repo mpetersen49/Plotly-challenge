e37690c91bd933f17996602bdad89216dc25cd35
mpetersen49 <mpetersen49@gmail.com>
2021-04-24 16:53:42 -0500

    Completed all code

--- static/js/app.js
+++ static/js/app.js
@@ -96,9 +96,7 @@ function updateDemoInfo(sampleId) {
             //console.log(`${key}: ${value}`);
             demoInfo.append("h6")
                 .text(`${key}: ${value}`);
-
         });        
-        
     });
 };
 
@@ -118,32 +116,51 @@ function updateGauge(sampleId) {
         console.log(wfreq);
 
         var gaugeData = {
-                domain: { x: [0, 1], y: [0, 1] },
                 value: wfreq,
                 title: {
-                    text: "Belly Button Washing Frequency"
+                    text: "<b>Belly Button Washing Frequency</b><br>Scrubs per Week"
                 },
                 type: "indicator",
                 mode: "gauge+number",
                 gauge: {
                     axis: {
-                        range: [null, 9]
-                    },
-                    steps: {
                         range: [null, 9],
-                        color: 'Greens'
-                    }
+                        tickmode: 'linear',
+                        tick0: 0,
+                        dtick: 1
+                    },
+                    steps: [
+                        {range: [0,1], color: "rgba(255, 255, 255, 0)"},
+                        {range: [1,2], color: "rgba(232, 226, 202, .5)"},
+                        {range: [2,3], color: "rgba(210, 206, 145, .5)"},
+                        {range: [3,4], color: "rgba(202, 209, 95, .5)"},
+                        {range: [4,5], color: "rgba(184, 205, 68, .5)"},
+                        {range: [5,6], color: "rgba(170, 202, 42, .5)"},
+                        {range: [6,7], color: "rgba(142, 178, 35 , .5)"},
+                        {range: [7,8], color: "rgba(110, 154, 22, .5)"},
+                        {range: [8,9], color: "rgba(50, 143, 10, 0.5)"}
+                    ]
                 }
             };
 
+                        "rgba(14, 127, 0, .5)",
+                        "rgba(50, 143, 10, 0.5)",
+                        "rgba(110, 154, 22, .5)",
+                        "rgba(142, 178, 35 , .5)",
+                        "rgba(170, 202, 42, .5)",
+                        "rgba(184, 205, 68, .5)",
+                        "rgba(202, 209, 95, .5)",
+                        "rgba(210, 206, 145, .5)",
+                        "rgba(232, 226, 202, .5)",
+                        "rgba(255, 255, 255, 0)"
+
         var gaugeArray = [gaugeData];
         
-        var gaugeLayout = { 
+        var gaugeLayout = {
             width: 600, 
             height: 500, 
             margin: { t: 0, b: 0 } 
         };
-        
         
         Plotly.newPlot('gauge', gaugeArray, gaugeLayout);
 
